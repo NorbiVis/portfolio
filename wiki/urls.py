010df63abfd84584ca8a7f04203733834031220c
NorbiVis <norbi_vis@yahoo.com>
2021-07-12 21:00:29 +0300

    layout and home page'

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import home_page
+from .views import home_page, new_page, random_page, pages
 
 app_name = 'wiki'
 
 urlpatterns = [
-    path('', home_page, name="home_page")
+    path('', home_page, name="home_page"),
+    path('new_page', new_page, name='new_page'),
+    path('wiki/<str:title>', pages, name='pages'),
+    path('random_page', random_page, name='random_page'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Post
+from .forms import PostForm
+from django.http import Http404
 
-# Create your views here.
 
 def home_page(request):
+    posts = Post.objects.all()
     return render(request, 'wiki/index.html', {
+        'posts': posts
+    })
+
+
+def new_page(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+    return render(request, 'wiki/new_page.html', {
+
+    })
+
+
+def pages(request, title):
+    try:
+        posts = Post.objects.get(title__iexact=title)
+    except:
+        raise Http404
+    return render(request, 'wiki/pages.html', {
+        'posts': posts
+    })
+
+
+def random_page(request):
+    return render(request, 'wiki/random_page.html', {
 
     })
