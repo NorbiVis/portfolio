0ef59467b0acbd15f7e11009cb3f7417438d70e7
NorbiVis <norbi_vis@yahoo.com>
2021-07-13 19:22:45 +0300

    project setup

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('wiki.urls')),
+    path('wiki/', include('wiki.urls')),
+    path('auction/', include('auction.urls')),
 ]
