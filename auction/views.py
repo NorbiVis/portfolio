fa80aa9b696b3a5ce971ef54b78823f85c5430a3
NorbiVis <norbi_vis@yahoo.com>
2021-07-15 18:15:01 +0300

    moved user

--- auction/views.py
+++ auction/views.py
@@ -4,34 +4,3 @@ from .forms import RegisterForm, LoginForm
 
 User = get_user_model()
 
-
-def register_view(request):
-    form = RegisterForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password1")
-        user = User.objects.create_user(username, email, password)
-        return redirect("/")
-    return render(request, 'auction/register.html', {
-        'form': form
-    })
-
-
-def login_view(request):
-    form = LoginForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user != None:
-            login(request, user)
-            return redirect("auction:index")
-    return render(request, 'auction/login.html', {
-        'form': form
-    })
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("auction:index")
