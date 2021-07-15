f88e2b7b7aa759dc422a6eb4fdb69a7eaad2e0ed
NorbiVis <norbi_vis@yahoo.com>
2021-07-15 18:26:15 +0300

    change branch

--- auction/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import register_view, login_view
-
-app_name = 'auction'
-
-urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login_view')
-]
