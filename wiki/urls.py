6d2e689e1af58628edd85a09327faf565033b1ef
NorbiVis <norbi_vis@yahoo.com>
2021-07-15 19:01:48 +0300

    app changes

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,8 +6,8 @@ app_name = 'wiki'
 urlpatterns = [
     path('', home_page, name="home_page"),
     path('new_page', new_page, name='new_page'),
-    path('wiki/<str:title>', pages, name='pages'),
+    path('<str:title>', pages, name='pages'),
     path('random_page', random, name='random_page'),
     path('search', search_pages, name="search"),
-    path('wiki/<str:title>/edit', edit_post, name='edit_post')
+    path('<str:title>/edit', edit_post, name='edit_post')
 ]
