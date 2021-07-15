b9787964dd23fb645cd27be9b3e11d6dac0a9cc2
NorbiVis <norbi_vis@yahoo.com>
2021-07-15 18:39:34 +0300

    moving styles and layout

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('wiki/', include('wiki.urls')),
-    path('auction/', include('auction.urls')),
 ]
